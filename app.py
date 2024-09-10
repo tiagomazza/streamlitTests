1a7e3ad9398208ca1eb9f01cfaf922c1fabb16ad
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-09-10 16:04:12 +0000

    new conection

--- app.py
+++ app.py
@@ -1,77 +1,226 @@
+import pandasql as psql
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
-from datetime import datetime, timedelta
-import pandas as pd
-import numpy as np
-import traceback
 
-try:
+st.subheader("📗 Google Sheets st.connection using Service Account")
+
+st.write("#### 1. API Reference")
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
     conn = st.connection("gsheets", type=GSheetsConnection)
-    st.success("Conexão com Google Sheets estabelecida com sucesso.")
-except Exception as e:
-    st.error(f"Erro ao conectar com Google Sheets: {e}")
-    st.error(traceback.format_exc())
+    st.write(conn)
+    st.help(conn)
 
-def load_existing_data(worksheet_name):
-    try:
-        existing_data = conn.read(worksheet=worksheet_name, ttl=5)
-        return existing_data.dropna(how="all")
-    except Exception as e:
-        st.error(f"Erro ao carregar dados da planilha '{worksheet_name}': {e}")
-        st.error(traceback.format_exc())
-        return pd.DataFrame()
+docs_url = (
+    "https://docs.streamlit.io/streamlit-community-cloud/get-started/"
+    "deploy-an-app/connect-to-data-sources/secrets-management"
+)
 
-# Interface do usuário
-st.sidebar.image("https://aborgesdoamaral.pt/wp-content/uploads/2021/04/marca-de-75-anos.png", use_column_width=True)
-pagina_selecionada = st.sidebar.radio("Selecione a página", ["✍🏽Marcação de Ponto", "🔍Consultas", "🔐Restrito"])
+st.write("#### 2. Initial setup")
+st.markdown(
+    f"""
+## Initial setup for CRUD mode
 
-try:
-    dados = conn.read(worksheet="Dados", usecols=["Pin", "Nome"], ttl=5)
-    admin_row = dados.loc[dados["Nome"] == "Admin"]
-    senha_admin = str(int(admin_row["Pin"].iloc[0])) if not admin_row.empty else None
-except Exception as e:
-    st.error(f"Erro ao carregar dados de administrador: {e}")
-    st.error(traceback.format_exc())
+1. Setup `.streamlit/secrets.toml` inside your Streamlit app root directory,
+check out [Secret management documentation]({docs_url}) for references.
 
-existing_data_reservations = load_existing_data("Folha")
+2. [Enable API Access for a
+Project](https://docs.gspread.org/en/v5.7.1/oauth2.html#enable-api-access-for-a-project)
+    * Head to [Google Developers Console](https://console.developers.google.com/) and
+      create a new project (or select the one you already have).
+    * In the box labeled “Search for APIs and Services”, search for “Google Drive API”
+      and enable it.
+    * In the box labeled “Search for APIs and Services”, search for “Google Sheets API”
+      and enable it.
+3. [Using Service
+   Account](https://docs.gspread.org/en/v5.7.1/oauth2.html#for-bots-using-service-account)
+    * Enable API Access for a Project if you haven't done it yet.
+    * Go to “APIs & Services > Credentials” and choose “Create credentials > Service
+      account key”.
+    * Fill out the form
+    * Click “Create” and “Done”.
+    * Press “Manage service accounts” above Service Accounts.
+    * Press on ⋮ near recently created service account and select “Manage keys” and then
+      click on “ADD KEY > Create new key”.
+    * Select JSON key type and press “Create”.
 
-if pagina_selecionada == "✍🏽Marcação de Ponto":
-    st.title("✍🏽Marcação de Ponto")
-    pin_digitado = st.text_input("Digite o seu PIN:")
+You will automatically download a JSON file with credentials. It may look like this:
+```
+{{
+    "type": "service_account",
+    "project_id": "api-project-XXX",
+    "private_key_id": "2cd … ba4",
+    "private_key": "-----BEGIN PRIVATE KEY-----\\nNrDyLw … jINQh/9\\n-----END PRIVATE KEY-----\\n",
+    "client_email": "473000000000-yoursisdifferent@developer.gserviceaccount.com",
+    "client_id": "473 … hd.apps.googleusercontent.com",
+    ...
+}}
+```
+Remember the path to the downloaded credentials file. Also, in the next step you'll need
+the value of client_email from this file.
 
-    if str(pin_digitado):
-        try:
-            dados = conn.read(worksheet="Dados", usecols=["Pin", "Nome"], ttl=5)
-            pin_int = int(float(pin_digitado))
-            
-            if pin_int in dados["Pin"].tolist():
-                nome = dados.loc[dados["Pin"] == pin_int, "Nome"].iloc[0]
-                st.write(f"😀 Bem-vindo, {nome}!")
-                st.write("")
+* **:red[Very important!]** Go to your
+spreadsheet and share it with a client_email from the step above. Just like you do with
+any other Google account. If you don't do this, you'll get a
+`gspread.exceptions.SpreadsheetNotFound` exception when trying to access this
+spreadsheet from your application or a script.
 
-                for button_text, button_name in [
-                    ("☕ Entrada Manhã", "Entrada Manhã")
-                ]:
-                    if st.button(button_text):
-                        try:
-                            submission_datetime = (datetime.now() + timedelta(hours=1)).strftime("%Y-%m-%d %H:%M:%S")
-                            new_row = {"Name": nome, "Button": button_name, "SubmissionDateTime": submission_datetime}
-                            new_rows = existing_data_reservations.to_dict(orient="records")
-                            new_rows.append(new_row)
-                            conn.update(worksheet="Folha", data=new_rows)
-                            st.success("Dados registrados com sucesso!")
-                        except Exception as e:
-                            st.error(f"Erro ao registrar {button_name}: {e}")
-                            st.error(traceback.format_exc())
-            else:
-                st.warning("Pin incorreto.")
-        except ValueError:
-            st.warning("Utilize somente números")
-        except Exception as e:
-            st.error(f"Erro inesperado: {e}")
-            st.error(traceback.format_exc())
+4. Inside `streamlit/secrets.toml` place `service_account` configuration from downloaded
+   JSON file, in the following format (where `gsheets` is your `st.connection` name):
 
-try:
-    entered_password = str(int(st.sidebar.text_input("Senha de Administrador", type="password", help="Digite a senha de administrador")))
-except ValueError:
-    st.sidebar.warning("Senha deve conter apenas números")
+```
+# .streamlit/secrets.toml
+[connections.gsheets]
+spreadsheet = "<spreadsheet-name-or-url>"
+
+# worksheet GID is used when using Public Spreadsheet URL, when using service_account
+# it will be picked as folder_id
+worksheet = "<worksheet-gid-or-folder-id>"
+
+# leave empty when using Public Spreadsheet URL, when using
+# service_account -> type = "service_account"
+type = ""
+
+project_id = ""
+private_key_id = ""
+private_key = ""
+client_email = ""
+client_id = ""
+auth_uri = ""
+token_uri = ""
+auth_provider_x509_cert_url = ""
+client_x509_cert_url = ""
+```
+
+"""
+)
+
+st.write("#### 3. Load DataFrame into Google Sheets")
+
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
+    # Create GSheets connection
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    # Demo Births DataFrame
+    df = psql.load_births()
+
+    # click button to update worksheet
+    # This is behind a button to avoid exceeding Google API Quota
+    if st.button("Create new worksheet"):
+        df = conn.create(
+            worksheet="Example 1",
+            data=df,
+        )
+        st.cache_data.clear()
+        st.rerun()
+
+    # Display our Spreadsheet as st.dataframe
+    st.dataframe(df.head(10))
+
+
+st.write("#### 4. Read Google WorkSheet as DataFrame")
+st.info(
+    "If the sheet has been deleted, press 'Create new worksheet' button above.",
+    icon="ℹ️",  # noqa: RUF001
+)
+
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
+    # Create GSheets connection
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    # Read Google WorkSheet as DataFrame
+    df = conn.read(
+        worksheet="Example 1",
+        usecols=[
+            0,
+            1,
+        ],  # specify columns which you want to get, comment this out to get all columns
+    )
+
+    # Display our Spreadsheet as st.dataframe
+    st.dataframe(df)
+
+st.write("#### 5. Update Google WorkSheet using DataFrame")
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
+    # Create GSheets connection
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    # Demo Meat DataFrame
+    df = psql.load_meat()
+
+    # click button to update worksheet
+    # This is behind a button to avoid exceeding Google API Quota
+    if st.button("Update worksheet"):
+        df = conn.update(
+            worksheet="Example 1",
+            data=df,
+        )
+        st.cache_data.clear()
+        st.rerun()
+
+    # Display our Spreadsheet as st.dataframe
+    st.dataframe(df.head(10))
+
+st.write("#### 6. Query Google WorkSheet with SQL and get results as DataFrame")
+st.info(
+    "Mutation SQL queries are in-memory only and do not results in the Worksheet update.",
+    icon="ℹ️",  # noqa: RUF001
+)
+
+
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
+    # Create GSheets connection
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    # make sure worksheet name is in double quota "", in our case it's "Example 1"
+    # DuckDB SQL dialect is supported
+    sql = 'select * from "Example 1"'
+
+    df = conn.query(sql=sql, ttl=3600)
+
+    # Display our SQL query results as st.dataframe
+    st.dataframe(df.head(10))
+
+st.write("#### 7. Clear/delete worksheet")
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
+    # Create GSheets connection
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    # click button to update worksheet
+    # This is behind a button to avoid exceeding Google API Quota
+    if st.button("Clear worksheet"):
+        conn.clear(worksheet="Example 1")
+        st.info("Worksheet Example 1 Cleared!")
+        st.cache_data.clear()
+        st.rerun()
+
+    # click button to delete worksheet using the underlying gspread API
+    # This is behind a button to avoid exceeding Google API Quota
+    if st.button("Delete worksheet"):
+        spreadsheet = conn.client._open_spreadsheet()  # type: ignore
+        worksheet = spreadsheet.worksheet("Example 1")
+        spreadsheet.del_worksheet(worksheet)
+        st.cache_data.clear()
+        st.rerun()
