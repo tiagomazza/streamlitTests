2bf293620f3215a49f938e1cb0450d35f73e8223
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-09-23 14:35:30 +0000

    df updated

--- app.py
+++ app.py
@@ -232,15 +232,14 @@ from streamlit_gsheets import GSheetsConnection
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
-
 df = conn.read(
     worksheet="Folha",
     ttl="10m",
     usecols=[0, 1],
     nrows=3,
 )
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
+
