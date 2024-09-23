86783d95da767ae724fb60679b332fe0871e9613
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-09-23 14:58:45 +0000

    without lines limits

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ df = conn.read(
     usecols=[0, 1],
 )
 
-# Exibe os dados existentes
-st.write("Dados existentes:")
-st.dataframe(df)
-
 # Cria um formulário para adicionar novos dados
 with st.form("add_data"):
     new_name = st.text_input("Nome")
@@ -35,17 +31,3 @@ with st.form("add_data"):
         
         st.success("Dados adicionados com sucesso!")
 
-# Lê os dados atualizados
-updated_df = conn.read(
-    worksheet="Folha",
-    ttl="0",  # Define ttl para 0 para forçar uma nova leitura
-    usecols=[0, 1],
-)
-
-# Exibe os dados atualizados
-st.write("Dados atualizados:")
-st.dataframe(updated_df)
-
-# Print results.
-for row in updated_df.itertuples():
-    st.write(f"{row.Name} has a :{row.Button}:")
