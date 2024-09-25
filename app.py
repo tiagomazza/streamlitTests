9daab9100aad4a406506dfbf5b97f3f78ae18797
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-09-25 09:53:38 +0000

    test restrict area

--- app.py
+++ app.py
@@ -279,8 +279,7 @@ try:
         df['Entrada Tarde'] = pd.to_datetime(df['Entrada Tarde'])
         df['Saída Tarde'] = pd.to_datetime(df['Saída Tarde'])
 
-        save_to_new_sheet(df)
-        fill_missing_data(df)
+        #fill_missing_data(df)
 
         grouped_data = df.groupby(['Data', 'Nome']).agg({
             'Entrada Manhã': 'first',
